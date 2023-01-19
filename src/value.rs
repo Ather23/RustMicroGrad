d0c3f149278910c9b43abaaf88485b93c72c5073
alif <alif@Alif.localdomain>
2023-01-19 01:41:12 -0500

    Adding logic to keep track of previous nodes

--- src/value.rs
+++ src/value.rs
@@ -1,55 +1,56 @@
 mod value {
     use ndarray::prelude::*;
 
-    #[derive(Clone)]
-    struct ValueProps {
-        Data: Option<ArrayD<f64>>,
+    #[derive(Clone, Debug)]
+    struct Value {
+        data: Option<ArrayD<f64>>,
+        prev_ops: Option<Vec<PrevOp>>,
     }
 
-    trait Value {
-        fn add<A, B>(&self, a: A, b: B) -> ArrayD<f64>
-        where
-            A: Into<ArrayD<f64>>,
-            B: Into<ArrayD<f64>>;
+    #[derive(Clone, Debug)]
+    struct PrevOp {
+        op: Value,
+    }
 
-        fn mult<A, B>(&self, a: A, b: B) -> ArrayD<f64>
-        where
-            A: Into<ArrayD<f64>>,
-            B: Into<ArrayD<f64>>;
-
+    trait IValue {
+        fn add(&self, b: Value) -> Value;
+        fn mult(&self, b: Value) -> Value;
+        fn prev_ops(&self, b: Value) -> Vec<PrevOp>;
         fn print(&self);
     }
 
-    impl Value for ValueProps {
-        fn add<A, B>(&self, a: A, b: B) -> ArrayD<f64>
-        where
-            A: Into<ArrayD<f64>>,
-            B: Into<ArrayD<f64>>,
-        {
-            let a = a.into();
-            let b = b.into();
-            a + b
+    impl IValue for Value {
+        fn add(&self, b: Value) -> Value {
+            let add = &self.data.clone().unwrap() + b.data.clone().unwrap();
+            let mut out = Value::new(&add);
+            out.prev_ops = Some(self.prev_ops(b));
+            return out;
         }
 
-        fn mult<A, B>(&self, a: A, b: B) -> ArrayD<f64>
-        where
-            A: Into<ArrayD<f64>>,
-            B: Into<ArrayD<f64>>,
-        {
-            let a = a.into();
-            let b = b.into();
-            a * b
+        fn mult(&self, b: Value) -> Value {
+            let mul = &self.data.clone().unwrap() * b.data.clone().unwrap();
+            let mut out = Value::new(&mul);
+            out.prev_ops = Some(self.prev_ops(b));
+            return out;
+        }
+
+        fn prev_ops(&self, b: Value) -> Vec<PrevOp> {
+            let mut prev_set: Vec<PrevOp> = Vec::new();
+            prev_set.push(PrevOp { op: self.clone() });
+            prev_set.push(PrevOp { op: b });
+            prev_set
         }
 
         fn print(&self) {
-            println!("Value(data={:?})", &self.Data);
+            println!("Value(data={:?})", &self.data);
         }
     }
 
-    impl ValueProps {
+    impl Value {
         fn new(data: &ArrayD<f64>) -> Self {
-            ValueProps {
-                Data: Some(data.clone()),
+            Value {
+                data: Some(data.clone()),
+                prev_ops: None,
             }
         }
     }
@@ -59,10 +60,13 @@ mod value {
         println!("Testing addition");
         let a = ArrayD::ones(vec![2, 1]);
         let b = ArrayD::ones(vec![2, 1]);
-        let val = ValueProps::new(&a);
-        let result = val.add(a, b);
-        assert_eq!(result.dim().as_array_view().dim(), 2);
-        assert_eq!(result.shape(), [2, 1]);
+
+        let val_a = Value::new(&a);
+        let val_b = Value::new(&b);
+        let result = val_a.add(val_b);
+        result.print();
+
+        println!("Previous ops {:?}", result.prev_ops);
     }
 
     #[test]
@@ -70,18 +74,35 @@ mod value {
         println!("Testing multiplication");
         let a = ArrayD::from_shape_vec(vec![4], vec![1.0, 2.0, 3.0, 4.0]).unwrap();
         let b = ArrayD::from_shape_vec(vec![4], vec![1.0, 2.0, 3.0, 4.0]).unwrap();
-        let val = ValueProps::new(&a);
-        let result = val.mult(a, b);
+
+        let val_a = Value::new(&a);
+        let val_b = Value::new(&b);
+        let result = val_a.mult(val_b);
         println!("{:?}", result);
-        assert_eq!(result.dim().as_array_view().dim(), 1);
-        assert_eq!(result.shape(), [4]);
+    }
+    #[test]
+    fn computation() {
+        println!("Testing multiplication");
+        let a = ArrayD::from_shape_vec(vec![1], vec![2.0]).unwrap();
+        let b = ArrayD::from_shape_vec(vec![1], vec![-3.0]).unwrap();
+        let c = ArrayD::from_shape_vec(vec![1], vec![10.0]).unwrap();
+
+        let val_a = Value::new(&a);
+        let val_b = Value::new(&b);
+        let val_c = Value::new(&c);
+        let result = val_a.mult(val_b).add(val_c);
+
+        println!("Previous ops {:?}", result.prev_ops);
+        let prev = result.prev_ops.unwrap()[0].op.to_owned().data.unwrap();
+        print!("{:?}", prev);
+        assert_eq!(prev[0], -6.)
     }
 
     #[test]
     fn print() {
         println!("Testing printing");
         let a = ArrayD::from_shape_vec(vec![4], vec![1.0, 2.0, 3.0, 4.0]).unwrap();
-        let val = ValueProps::new(&a);
+        let val = Value::new(&a);
         val.print();
     }
 }
